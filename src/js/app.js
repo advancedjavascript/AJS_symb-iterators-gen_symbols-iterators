ae0395d512bec81d4f8a0d70b4d654fc6acc26ca
kate_gaw <ekaterinagaw@gmail.com>
2019-12-24 13:25:00 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,58 @@
 export default class Team {
-    constructor() {
-        this.team = [{
-                name: 'Лучник',
-                type: 'Bowman',
-                health: 100,
-                level: 1,
-                attack: 25,
-                defence: 25,
-            },
-            {
-                name: 'Мечник',
-                type: 'Swordsman',
-                health: 100,
-                level: 1,
-                attack: 40,
-                defence: 10,
-            },
-            {
-                name: 'Маг',
-                type: 'Magician',
-                health: 100,
-                level: 1,
-                attack: 10,
-                defence: 40,
-            },
-            {
-                name: 'Демон',
-                type: 'Daemon',
-                health: 100,
-                level: 1,
-                attack: 10,
-                defence: 40,
-            },
-        ];
-    }
+  constructor() {
+    this.team = [{
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 100,
+      level: 1,
+      attack: 25,
+      defence: 25,
+    },
+    {
+      name: 'Мечник',
+      type: 'Swordsman',
+      health: 100,
+      level: 1,
+      attack: 40,
+      defence: 10,
+    },
+    {
+      name: 'Маг',
+      type: 'Magician',
+      health: 100,
+      level: 1,
+      attack: 10,
+      defence: 40,
+    },
+    {
+      name: 'Демон',
+      type: 'Daemon',
+      health: 100,
+      level: 1,
+      attack: 10,
+      defence: 40,
+    },
+    ];
+  }
 
-    [Symbol.iterator]() {
-        const teams = this.team;
-        let index = 0;
+  [Symbol.iterator]() {
+    const teams = this.team;
+    let index = 0;
 
-        return {
-            next() {
-                const result = {
-                    value: undefined,
-                    done: true,
-                };
-
-                if (index < teams.length) {
-                    result.value = teams[index];
-                    result.done = false;
-                    index += 1;
-                }
-                return result;
-            },
+    return {
+      next() {
+        const result = {
+          value: undefined,
+          done: true,
         };
-    }
+
+        if (index < teams.length) {
+          result.value = teams[index];
+          result.done = false;
+          index += 1;
+        }
+        return result;
+      },
+    };
+  }
 }
